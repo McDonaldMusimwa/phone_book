b0308b4b9c07aec8b5a96a26be37e4c6c8e78ada
McDonald <mus22003@byui.edu>
2023-04-24 20:56:38 +0200

    app crud
    added contacts add,put,delete

--- controller/index.js
+++ controller/index.js
@@ -1,30 +1,32 @@
-const { ObjectId } = require('mongodb');
-const mongodb = require('../db/connection');
-//const Objectid = require("mongodb").ObjectId;
+const { ObjectId } = require("mongodb");
+const mongodb = require("../db/connection");
+//const postSchema = require('../models/model')
 
-const getAllContacts = async (req, res, next) => {
+//Read all contacts
+const getAllContacts = async (req, res) => {
   const contacts = await mongodb
     .getDb()
-    .db('phonebook')
-    .collection('contacts')
+    .db("phonebook")
+    .collection("contacts")
     .find();
   contacts.toArray().then((list) => {
-    res.setHeader('Content-Type', 'Application/json');
-    res.status('200').json(list);
+    res.setHeader("Content-Type", "Application/json");
+    res.status("200").json(list);
   });
 };
 
+//get one contact
 const getOneContact = async (req, res, next) => {
   try {
     const contactId = new ObjectId(req.params.id);
     const contact = await mongodb
       .getDb()
-      .db('phonebook')
-      .collection('contacts')
+      .db("phonebook")
+      .collection("contacts")
       .findOne({ _id: contactId });
 
     if (!contact) {
-      return res.status(404).json({ message: 'Contact not found' });
+      return res.status(404).json({ message: "Contact not found" });
     }
 
     res.status(200).json(contact);
@@ -33,4 +35,79 @@ const getOneContact = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllContacts, getOneContact };
+//Add one contact
+const addOneContact = async (req, res) => {
+  console.log(req.body);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    birthDay: req.body.birthday,
+    favorateColor: req.body.favorateColor,
+  };
+
+  const response = await mongodb
+    .getDb()
+    .db("phonebook")
+    .collection("contacts")
+    .insertOne(contact);
+  if (response.acknowledged) {
+    res.status(200).json(response);
+  } else {
+    res.status(400).send(`${contact.firstName} failed to add to the phonebook`);
+  }
+};
+
+//Update One contact
+const modifyOneContact = async (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    birthDay: req.body.birthDay,
+    favorateColor: req.body.favorateColor,
+  };
+
+  const response = await mongodb
+    .getDb()
+    .db("phonebook")
+    .collection("contacts")
+    .updateOne({ _id: contactId }, { $set: contact });
+  if (response.acknowledged) {
+    res.status(200).json(response);
+  } else {
+    res.status(400).send(`${contact.firstName} failed to add to the phonebook`);
+  }
+};
+
+//Delete One contact
+const deleteOneContact = async (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const contact = await mongodb
+    .getDb()
+    .db("phonebook")
+    .collection("contacts")
+    .findOne({ _id: contactId });
+  contact.toArray().json();
+  const response = await mongodb
+    .getDb()
+    .db("phonebook")
+    .collection("contacts")
+
+    .deleteOne({ _id: contactId });
+  if (response.acknowledged) {
+    res.status(200).json(response);
+  } else {
+    res.status(400).send(`${contact.firstName} failed to add to the phonebook`);
+  }
+};
+
+module.exports = {
+  getAllContacts,
+  getOneContact,
+  addOneContact,
+  deleteOneContact,
+  modifyOneContact,
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,13 @@
-
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const getContacts = require('../controller/index');
+const getContacts = require("../controller/index");
 
-router.get('/', getContacts.getAllContacts);
-
-router.get('/:id', getContacts.getOneContact);
+router.get("/", getContacts.getAllContacts);
+router.post("/addcontact", getContacts.addOneContact);
+router.put("/modify/:id", getContacts.modifyOneContact);
+router.delete("/delete/:id", getContacts.deleteOneContact);
+router.get("/:id", getContacts.getOneContact);
 
 module.exports = router;
