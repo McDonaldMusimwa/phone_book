93f9b5fe46f262204662bf40bd344e50611889d1
McDonald <mus22003@byui.edu>
2023-04-25 14:12:44 +0200

    edited readme

--- db/connection.js
+++ db/connection.js
@@ -1,21 +1,20 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const mongodb = require("mongodb");
+const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const url = process.env.URL;
 
 let _db;
-const mongoConnect =async (callback) => {
+const mongoConnect = async (callback) => {
   if (_db) {
-    console.log("Db is already initialized!");
+    console.log('Db is already initialized!');
     return callback(null, _db);
   }
   await MongoClient.connect(url)
     .then((client) => {
-      
       _db = client;
-      console.log("database connected");
-      callback( _db);
+      console.log('database connected');
+      callback(_db);
     })
     .catch((err) => {
       console.log(err);
@@ -24,7 +23,7 @@ const mongoConnect =async (callback) => {
 
 const getDb = () => {
   if (!_db) {
-    throw Error("Db not initialized");
+    throw Error('Db not initialized');
   }
   return _db;
 };
