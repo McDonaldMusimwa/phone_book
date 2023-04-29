824dbb8dea36868ace3d41dbe704dfc74857b50a
McDonald <mus22003@byui.edu>
2023-04-29 21:57:32 +0200

    changed add contact endpoint

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const getContacts = require("../controller/index");
 
 router.get("/contacts", getContacts.getAllContacts);
-router.post("/addcontact", getContacts.addOneContact);
+router.post("/contacts", getContacts.addOneContact);
 router.put("/modify/:id", getContacts.modifyOneContact);
 router.delete("/delete/:id", getContacts.deleteOneContact);
 router.get("/:id", getContacts.getOneContact);
